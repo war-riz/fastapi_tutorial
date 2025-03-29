d1b9e2869f7326025c5d945c387086d35ff980ee
war-riz <kehindewaris54@gmail.com>
2025-03-29 23:45:33 +0100

    relationship between models

--- blog/schemas.py
+++ blog/schemas.py
@@ -3,3 +3,25 @@ from pydantic import BaseModel
 class Blog(BaseModel):
     title: str
     content: str
+    user_id: int
+    class Config:
+        orm_mode = True
+
+class User(BaseModel):
+    username: str
+    email: str
+    password: str
+
+class ShowUser(BaseModel):
+    username: str
+    email: str
+    blogs: list[Blog] = []
+    class Config:
+        orm_mode = True
+
+class ShowBlog(BaseModel):
+    title: str
+    content: str
+    user: ShowUser
+    class Config:
+        orm_mode = True
