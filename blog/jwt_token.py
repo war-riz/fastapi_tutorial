e72000ad0d31c353c2bb6a8aff3b3c6362c7a66a
war-riz <kehindewaris54@gmail.com>
2025-03-31 12:24:32 +0100

    creating jwt token

---
+++ blog/jwt_token.py
@@ -0,0 +1,18 @@
+from typing import Optional
+from datetime import timedelta, datetime
+from jose import jwt
+
+SECRET_KEY = "e6dcefd67df0e35201c11ceb48834d2a303ae87fe603470e133b624d79ac5f48"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + timedelta(minutes=expires_delta)
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
